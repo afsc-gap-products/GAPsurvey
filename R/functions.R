7fa90f0faf367f0de0ec2b9ec6d5effae00d3d7b
emilyhmarkowitz <emily.markowitz@noaa.gov>
2021-03-30 15:55:21 -0700

    new catchdata() and fix to lengthdata

--- R/functions.R
+++ R/functions.R
@@ -39,6 +39,7 @@
 #'    VERSION_NUMBER = 123,
 #'    SERIAL_NUMBER = 123,
 #'    path_in = paste0(path_in0, "/bvdr2btd/"),
+#'    path_out = getwd(),
 #'    filename_add = "new", # is this important?
 #'    quiet = TRUE)
 TEDtoBTD <- function(
@@ -83,7 +84,7 @@ TEDtoBTD <- function(
   colnames(tet.file)=c("date","temp")
   # str(ted.file)
   # str(tet.file)
-  merged=merge(ted.file,tet.file,all=T)
+  merged<-base::merge(ted.file,tet.file,all=T)
   # str(merged)
   # head(merged)
 
@@ -236,152 +237,7 @@ CTDXMLtoBTD <- function(
 
 
 
-#' Convert raw CTD files to BTD/BTH format
-#'
-#' To RUN a function, put curser on line you want to run and press CTRL+R
-#'
-#' When first open "Survey R Functions" the function library("tcltk")
-#' MUST be run ONCE
-#' As long as R console remains open, the function library("tcltk")
-#' does not need to be run a second time
-#'
-#' library("tcltk")
-#'
-#' The "CTDtoBTD()" function converts an exported CSV file from the Teledyne CTD Program into BTD and BTH files
-#' File format for the CSV file must be DATE,TIME,TEMP,DEPTH (mm-dd-yyyy,hh:mm:ss.ss,(C),(m))
-#' Program prompts you for: Vessel code, Cruise number, Haul number, CTD model number,
-#' Version number (=999), CTD serial number, and then opens window for you  to select file from any directory.
-#' The BTH and BTD files are written to the C: \ NM directory
-#'
-#' @param VESSEL Optional. Default = NA. The vessel number (e.g., 94). If NA or not called in the function, a prompt will appear asking for this data.
-#' @param CRUISE Optional. Default = NA. The cruise number, which is usually the year date (e.g., 201901). If NA or not called in the function, a prompt will appear asking for this data.
-#' @param HAUL Optional. Default = NA. The haul number, aka the iterative number of this haul (e.g., 3). If NA or not called in the function, a prompt will appear asking for this data.
-#' @param DATE Optional. Default = NA. The date in MM/DD/YYYY format (e.g., "06/02/2019"). If NA or not called in the function, a prompt will appear asking for this data.
-#' @param MODEL_NUMBER Optional. Default = NA. The model number of the CTD (I think) (e.g., 123) If NA or not called in the function, a prompt will appear asking for this data. # TOLEDO
-#' @param VERSION_NUMBER Optional. Default = NA. The version number of the CTD (I think) (e.g., 123) If NA or not called in the function, a prompt will appear asking for this data. # TOLEDO
-#' @param SERIAL_NUMBER Optional. Default = NA. The serial number of the CTD (I think) (e.g., 123) If NA or not called in the function, a prompt will appear asking for this data. # TOLEDO
-#' @param path_in Optional. Default = "./., or the local working directory but any path (as a string) may be entered.
-#' @param path_out Optional. The default is the local working directory but may be specified with a string.
-#' @param filename_add Optional. Default = "new". This string will be added to the name of the outputed file. Here, you can additional information that may make this file helpful to find later.
-#' @param quiet Optional logical TRUE/FALSE. Default = FALSE. If FALSE, will print a statement or a pop-up window will let the user know where the file has been saved to.
-#'
-#' @return A .BTH and .BTD file to the path_out directory.
-#'
-#' @importFrom magrittr %>%
-#' @return
-#' @export
-#'
-#' @examples
-#' path_in0 <- system.file("exdata", package = "GAPsurvey")
-#' # CTDtoBTD(
-#' #     VESSEL = 94,
-#' #     CRUISE = 201901,
-#' #     HAUL = 3,
-#' #     DATE = "06/02/2019",
-#' #     MODEL_NUMBER = 123,
-#' #     VERSION_NUMBER = 123,
-#' #     SERIAL_NUMBER = 123,
-#' #     path_in = paste0(path_in0, "ctd2btd/6-02-19/DATA01.ctd"),
-#' #     path_out = path_out0,
-#' #     filename_add = "", # is this important?
-#' #     quiet = TRUE)
-CTDtoBTD <- function(
-  VESSEL = NA,
-  CRUISE = NA,
-  HAUL = NA,
-  DATE = NA,
-  MODEL_NUMBER = NA,
-  VERSION_NUMBER = NA,
-  SERIAL_NUMBER = NA,
-  path_in = NA,
-  path_out = "./",
-  filename_add = "",
-  quiet = FALSE){
 
-  if (is.na(VESSEL)){ VESSEL <- readline("Type vessel code:  ") }
-  if (is.na(CRUISE)){ CRUISE <- readline("Type cruise number:  ") }
-  if (is.na(HAUL)){ HAUL <- readline("Type haul number:  ") }
-  if (is.na(DATE)){ DATE <- readline("Type date of haul (MM/DD/YYYY):  ") }
-  if (is.na(MODEL_NUMBER)){ MODEL_NUMBER <- readline("Type model number:  ") }
-  if (is.na(VERSION_NUMBER)){ VERSION_NUMBER <- readline("Type version number:  ") }
-  if (is.na(SERIAL_NUMBER)){ SERIAL_NUMBER <- readline("Type serial number of CTD:  ") }
-
-  HAUL <- as.numeric(HAUL)
-  shaul <- numbers0(x = HAUL, number_places = 4)
-
-  if (is.na(path_in)){
-    file.name <- file.choose()
-  } else {
-    # check if path exists #TOLEDO
-    file.name <- path_in
-  }
-
-  # ctd.names <- read.table(file = file.name, nrows = 2, header = FALSE)
-  ctd.names <- readr::read_csv(file.name, n_max = 0) %>% names()
-
-  # ctd.file <- utils::read.csv(file = file.name, col.names = ctd.names, skip = 2)
-  # ctd.file <- ctd.file[,c(DATE, TIME, TEMP, DEPTH)]
-  ctd.file <- readr::read_csv(file.name, col_names = ctd.names, skip = 2) %>%
-    dplyr::select(DATE, TIME, TEMP, DEPTH)
-  # ctd.file <- read.ctd(file = file.name)
-
-  ctd.file <- ctd.file %>%
-    dplyr::mutate(DATE = lubridate::mdy(DATE)) %>%
-    dplyr::mutate(DATE_TIME = lubridate::ymd_hms(paste(DATE, TIME))) %>%
-    dplyr::mutate(AKDT = lubridate::with_tz(DATE_TIME,tzone="Etc/GMT+8")) %>%
-    dplyr::mutate(AKDT = gsub("-","/",AKDT))
-
-  date_f <- format(as.Date(ctd.file$AKDT, '%Y/%m/%d'), "%m/%d/%Y")
-  time_f <- stringr::str_sub(ctd.file$AKDT, start = 12)
-  dt_f <- paste(date_f,time_f)
-
-  ctd.file <- ctd.file %>%
-    dplyr::mutate(AKDT_formatted = dt_f)
-
-  DATE_TIME <- ctd.file$AKDT_formatted
-
-  HOST_TIME=max(DATE_TIME)
-  LOGGER_TIME=max(DATE_TIME)
-  LOGGING_START=min(DATE_TIME)
-  LOGGING_END=max(DATE_TIME)
-  TEMPERATURE=round(ctd.file$TEMP,4)
-  DEPTH=round(ctd.file$DEPTH,4)
-  SAMPLE_PERIOD=3
-  NUMBER_CHANNELS=2
-  NUMBER_SAMPLES=0
-  MODE=2
-
-  new.BTD=as.data.frame(cbind(VESSEL,CRUISE,HAUL,SERIAL_NUMBER,DATE_TIME,TEMPERATURE,DEPTH))
-  new.BTH=cbind(VESSEL,CRUISE,HAUL,MODEL_NUMBER,VERSION_NUMBER,SERIAL_NUMBER,
-                HOST_TIME,LOGGER_TIME,LOGGING_START,LOGGING_END,SAMPLE_PERIOD,NUMBER_CHANNELS,
-                NUMBER_SAMPLES,MODE)
-
-  #haa=head(new.BTD,100)
-  idx=which(duplicated(new.BTD$DATE_TIME)==FALSE)
-  new.BTD=new.BTD[idx,]
-
-  HAUL=as.numeric(HAUL)
-  #head(new.BTD)
-  #return(head(new.BTD))
-  if(HAUL<10){
-    pre.haul="HAUL000"}
-  if(HAUL>9 & HAUL<100){
-    pre.haul="HAUL00"}
-  if(HAUL>99){
-    pre.haul="HAUL0"}
-
-  btd.name=paste(pre.haul,HAUL,"_new.BTD",sep="")
-  bth.name=paste(pre.haul,HAUL,"_new.BTH",sep="")
-  utils::write.csv(new.BTD,btd.name,quote=F,row.names=F)
-  utils::write.csv(new.BTH,bth.name,quote=F,row.names=F)
-
-
-  if(!quiet){
-    tcltk::tkmessageBox(title = "Message",
-                 message = paste0("Your new files are saved to the folder ", path_out),
-                 icon = "info", type = "ok")
-  }
-}
 
 
 
@@ -423,6 +279,7 @@ CTDtoBTD <- function(
 #'     HAUL = 3,
 #'     DATE = "06/06/2017",
 #'     path_in = paste0(path_in0, "/log2gps/06062017.log"),
+#'     path_out = getwd(),
 #'     filename_add = "",
 #'     quiet = TRUE)
 LOGtoGPS <- function(
@@ -511,7 +368,7 @@ LOGtoGPS <- function(
 #' @export
 lengthData <- function(haul, dsnTablet, dsnDataEnt) {
 
-  PolySpecies <- utils::data("PolySpecies")
+  PolySpecies <- GAPsurvey::PolySpecies
 
   options(stringsAsFactors = F)
 
@@ -548,7 +405,7 @@ lengthData <- function(haul, dsnTablet, dsnDataEnt) {
   entLengthFreq <- stats::aggregate.data.frame(tabRawLength$FREQUENCY,by=tabRawLength[c("HAUL","POLY_SPECIES_CODE","SEX","LENGTH")], FUN = sum)
   colnames(entLengthFreq)[5] <- "FREQUENCY"
 
-  entLengthFreq <- merge(entLengthFreq, PolySpecies, by = "POLY_SPECIES_CODE")[,c("HAUL","SPECIES_CODE","SEX","LENGTH","FREQUENCY")]
+  entLengthFreq <- base::merge(entLengthFreq, PolySpecies, by = "POLY_SPECIES_CODE")[,c("HAUL","SPECIES_CODE","SEX","LENGTH","FREQUENCY")]
   entLengthFreq$SUBSAMPLE_TYPE <- 1L
   entLengthFreq$LENGTH_TYPE <- NA
 
@@ -672,32 +529,32 @@ benthicData <- function(haul, dsnTablet, dsnDataEnt) {
 #' # catchData()
 catchData <- function(haul, dsnTablet, dsnDataEnt, importLength=TRUE) {
   options(stringsAsFactors = F)
-
+  
   # Grab data_ent formatted tablet data
   catchFile <- list.files(path=dsnTablet, pattern=paste0("DATAENT_CATCH_",sprintf("%04d", haul)))
-
+  
   if (length(catchFile) != 1) {
     stop("C1: Data Ent Catch file missing or duplicate")
   } else {
-    tabCatch <- utils::read.csv(file.path(dsnTablet,catchFile))
+    tabCatch <- read.csv(file.path(dsnTablet,catchFile))
   }
   tabCatch$seq <- seq(1:nrow(tabCatch))
-
-
+  
+  
   if (!is.character(tabCatch$SAMPLED_ALL)) {
     stop("C3: SAMPLED_ALL flag has not been set for any species in this haul, correct and try again.")
   }
-
+  
   if (anyNA(tabCatch$SAMPLED_ALL)) {
     stop("C3b: SAMPLED_ALL flag has not been set for one or more species in this haul, correct on tablet and try again.")
   }
-
+  
   # If importLength = TRUE, upload length files first
   if (importLength) {
     lengthFreq <- lengthData(haul,dsnTablet, dsnDataEnt)
-
-    lengthSum <- stats::aggregate.data.frame(lengthFreq$FREQUENCY,by=lengthFreq[c("SPECIES_CODE")], FUN = sum)
-
+    
+    lengthSum <- aggregate.data.frame(lengthFreq$FREQUENCY,by=lengthFreq[c("SPECIES_CODE")], FUN = sum)
+    
     halibut <- lengthFreq[lengthFreq$SPECIES_CODE==10120,]
     if (nrow(halibut) > 0) {
       halibutWeight <- sapply(halibut$LENGTH,FUN= function(x) 0.00237*x^3.24) * halibut$FREQUENCY/1000000
@@ -710,61 +567,61 @@ catchData <- function(haul, dsnTablet, dsnDataEnt, importLength=TRUE) {
                                   VOUCHER_NUMBER = NA,
                                   seq = 0
       )
-
+      
       tabCatch <- rbind(halibutWeight, tabCatch)
     }
-
+    
     # Check for lengths not incatch
     noCatch <- setdiff(lengthFreq$SPECIES_CODE,tabCatch$SPECIES_CODE)
     if (!sum(noCatch)==0) {
       stop(paste0("C5: there is no catch data for these species in Length: ", paste(noCatch, collapse=", "),". Re-run with importLength=FALSE and fix in data_ent.mdb"))
     }
-
-
+    
+    
     allCatch <- merge(tabCatch, lengthSum, by = "SPECIES_CODE", all=T)
-
+    
     # if (nrow(allCatch) != nrow(lengthSum)) {
     #   stop("C4: There are species in the length file not found in the catch, correct and try again.")
     # } else {
     #   allCatch$SUBSAMPLE_NUMBERS <- ifelse(is.na(allCatch$x),allCatch$SUBSAMPLE_NUMBERS,allCatch$x)
     #   tabCatch <- subset(allCatch, select = -x)
     # }
-
+    
     # Add subsample numbers from length to catch, then re-sort from original seq
     allCatch$SUBSAMPLE_NUMBERS <- ifelse(is.na(allCatch$x),allCatch$SUBSAMPLE_NUMBERS,allCatch$x)
     allCatch <- allCatch[order(allCatch$seq),]
-
+    
     tabCatch <- subset(allCatch, select = c(SPECIES_CODE,SPECIES_NAME,NONSUB_WEIGHT,SUBSAMPLE_WEIGHT,SUBSAMPLE_NUMBERS,SAMPLED_ALL,VOUCHER_NUMBER))
   } else {
     message("Length data not processed, handle subsample numbers manually in data_ent.mdb")
-
+    
     # Remove Seq column
     tabCatch <- subset(tabCatch, select = c(SPECIES_CODE,SPECIES_NAME,NONSUB_WEIGHT,SUBSAMPLE_WEIGHT,SUBSAMPLE_NUMBERS,SAMPLED_ALL,VOUCHER_NUMBER))
-
+    
   }
-
+  
   # Grab catch header files
   catchHeader <- list.files(path=dsnTablet, pattern=paste0("RAW_CATCH_HAUL_",sprintf("%04d", haul)))
   if (length(catchHeader) != 1) {
     stop("C4: Raw Catch Haul file missing or duplicated")
   } else {
-    tabCatchHeader <- utils::read.csv(file.path(dsnTablet,catchHeader))[,c("HAUL","WEIGHT","HUNDRED_PERCENT_SAMPLED")]
+    tabCatchHeader <- read.csv(file.path(dsnTablet,catchHeader))[,c("HAUL","WEIGHT","HUNDRED_PERCENT_SAMPLED")]
   }
   names(tabCatchHeader) <- c("HAUL","TOTAL_CATCH_WEIGHT","HUNDRED_PERCENT_SAMPLED")
-
+  
   # Test data_ent for catch header data
   CATCH_HEADER <- selectDataEnt(dsnDataEnt, "select * from CATCH_HEADER")
   if (nrow(CATCH_HEADER[CATCH_HEADER$HAUL==haul,]) > 0) {
     stop("C2: CATCH_HEADER data already exists for this haul, abort!")
   }
-
+  
   # If not 100% sampled, ask for catch subsample type
   if (tabCatchHeader$HUNDRED_PERCENT_SAMPLED == 0) {
     ADPsubsample <- selectDataEnt(dsnDataEnt, "select * from ADP_CATCH_SUBSAMPLE")
     print(ADPsubsample)
     subsample <- readline(prompt="Enter subsample code: ")
     tabCatchHeader$SUBSAMPLE_CODE <- subsample
-
+    
     # Handle SAME_PROPORTION flags based on SAMPLED_ALL flags
     if (sum(tabCatch$SAMPLED_ALL=='N')==0) {
       tabCatchHeader$SAME_PROPORTION <- 1
@@ -776,14 +633,14 @@ catchData <- function(haul, dsnTablet, dsnDataEnt, importLength=TRUE) {
     tabCatchHeader$SAME_PROPORTION <- -1
     tabCatchHeader$TOTAL_CATCH_WEIGHT <- 0
   }
-
+  
   # Format header for data_ent
   entCatchHeader <- subset(tabCatchHeader, select = c(HAUL,SUBSAMPLE_CODE,SAME_PROPORTION,TOTAL_CATCH_WEIGHT))
-
+  
   # Get starting index number from CATCH
   i_catch <- as.numeric(selectDataEnt(dsnDataEnt, "select max(ENTRY_ORDER) from CATCH") + 1)
   i_catch <- ifelse(is.na(i_catch), 1, i_catch)
-
+  
   # Do math for entCatch
   calcCatch <- tabCatch
   calcCatch$NONSUB_WEIGHT[is.na(calcCatch$NONSUB_WEIGHT)] <- 0
@@ -793,7 +650,7 @@ catchData <- function(haul, dsnTablet, dsnDataEnt, importLength=TRUE) {
   calcCatch$CALCULATED_NUMBERS <- round(calcCatch$SUBSAMPLE_NUMBERS + (calcCatch$NONSUB_WEIGHT/calcCatch$AVERAGE_WEIGHT), digits=0)
   calcCatch$ENTRY_ORDER <- seq(from=i_catch, length.out=nrow(calcCatch))
   calcCatch$HAUL <- haul
-
+  
   # Look for mix data
   mixes <- grep("Mix", tabCatch$SPECIES_NAME)
   if (length(mixes) == 0) {
@@ -802,20 +659,20 @@ catchData <- function(haul, dsnTablet, dsnDataEnt, importLength=TRUE) {
     entCatch <- calcCatch[1:(mixes-1),]
     entMixHeader <- calcCatch[mixes,]
     entMix <- calcCatch[(mixes+1):nrow(calcCatch),]
-
+    
     # Set up Mix calculations
     entMixHeader$SPECIES_CODE <- 99000
     entMixHeader$SUBSAMPLE_WEIGHT <- sum(entMix$SUBSAMPLE_WEIGHT)
     entMixHeader$CALCULATED_WEIGHT <- entMixHeader$SUBSAMPLE_WEIGHT + entMixHeader$NONSUB_WEIGHT
     entCatch <- rbind(entCatch,entMixHeader)
-
+    
   } else {
     for (i in 1:length(mixes)) {
       stop("Too many mixes!")
     }
-
+    
   }
-
+  
   # Total weight for extrapolation (minus sampled all)
   if (entCatchHeader$SUBSAMPLE_CODE==1) {
     entCatch$TOTAL_WEIGHT <- entCatch$CALCULATED_WEIGHT
@@ -826,36 +683,36 @@ catchData <- function(haul, dsnTablet, dsnDataEnt, importLength=TRUE) {
     entCatch$TOTAL_WEIGHT <- ifelse(entCatch$SAMPLED_ALL == 'Y', entCatch$CALCULATED_WEIGHT, (entCatch$CALCULATED_WEIGHT/extrapWeight * entCatchHeader$TOTAL_CATCH_WEIGHT))
     entCatch$TOTAL_NUMBERS <- round(entCatch$TOTAL_WEIGHT/entCatch$AVERAGE_WEIGHT, digits=0)
   }
-
+  
   entCatch[entCatch == Inf] <- NA
-
+  
   # Format mixture for data ent
   if (exists("entMixHeader")) {
     i_mix <- as.numeric(selectDataEnt(dsnDataEnt, "select max(ENTRY_ORDER) from MIXTURE") + 1)
     i_mix <- ifelse(is.na(i_mix), 1, i_mix)
-
+    
     entMixHeader$TOTAL_WEIGHT <- entCatch[grep("Mix", entCatch$SPECIES_NAME),"TOTAL_WEIGHT"]
-
+    
     entMix$ENTRY_ORDER <- seq(from=i_mix, length.out=nrow(entMix))
     entMix$TOTAL_WEIGHT <- if (entMixHeader$SAMPLED_ALL == 'Y') entMix$CALCULATED_WEIGHT else (entMix$CALCULATED_WEIGHT/sum(entMix$CALCULATED_WEIGHT)*entMixHeader$TOTAL_WEIGHT)
     entMix$TOTAL_NUMBERS <- round(entMix$TOTAL_WEIGHT/entMix$AVERAGE_WEIGHT, digits=0)
     entMix <- subset(entMix, select = c(HAUL,SPECIES_CODE,SPECIES_NAME,NONSUB_WEIGHT,SUBSAMPLE_WEIGHT,SUBSAMPLE_NUMBERS,CALCULATED_WEIGHT,
                                         CALCULATED_NUMBERS,AVERAGE_WEIGHT,ENTRY_ORDER,TOTAL_WEIGHT,TOTAL_NUMBERS))
     entMix[entMix == Inf] <- NA
-
+    
     entMixHeader$MIXTURE_TYPE_CODE <- 2
     entMixHeader$MIXTURE_SAMPLED_ALL <- ifelse(entMixHeader$SAMPLED_ALL=='Y',-1,0)
     entMixHeader <- subset(entMixHeader, select = c(HAUL,MIXTURE_TYPE_CODE,NONSUB_WEIGHT,MIXTURE_SAMPLED_ALL))
     names(entMixHeader) <- c("HAUL","MIXTURE_TYPE_CODE","MIXTURE_SAMPLE_WEIGHT","MIXTURE_SAMPLED_ALL")
-
+    
     # Convert zeros to NULLs for DB
     entMix[entMix == 0] <- NA
     # entMixHeader[entMixHeader == 0] <- NA
-
+    
     writeDataEnt(dsnDataEnt,entMixHeader,"MIXTURE_HEADER")
     writeDataEnt(dsnDataEnt,entMix,"MIXTURE")
   }
-
+  
   # Upload catch data
   # Convert zeros to NULLs for DB
   # entCatchHeader[entCatchHeader == 0] <- NA
